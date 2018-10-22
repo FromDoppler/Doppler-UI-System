3d8b90a323e0a9c25891e976f1cc594ab8fb763a
IgnacioRodrigues <ignaciorodriguesoficial@gmail.com>
2018-10-22 12:03:53 -0300

    complete structure base

---
+++ atomic-email-system/js/scroll.js
@@ -0,0 +1,10 @@
+
+  $(".scrollTo").on('click', function(e)
+  {
+    e.preventDefault();
+    var target = $(this).attr('href');
+    $('html, body').animate(
+    {
+      scrollTop: ($(target).offset().top)
+    }, 2000);
+  });
