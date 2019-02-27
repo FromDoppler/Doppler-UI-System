88b414ca79d6b9eb48dcbce05e5b20bcf7b96587
gbaamonde <gusvodesign@gmail.com>
2019-02-27 12:34:11 -0300

    feat: add styles for modal and form user-menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,21 +3,36 @@ $("body").on({
     var submenu = $(this).find(".sub-menu");
     if (submenu && submenu[0].children.length) {
       $('.header-main').addClass('header-open');
-    }  
+    }
   },
   mouseleave: function() {
     $('.header-main').removeClass('header-open');
   }
 }, ".menu-main .submenu-item");
 
-$("body").on('click', '#user-menu--open', function(){
+$("body").on('click', '#user-menu--open', function() {
   $('.user-menu').toggleClass('open');
 });
 
-$("body").on('click', '#open-menu', function(){
+$("body").on('click', '#open-menu', function() {
   $('.header-main').addClass('open');
 });
 
-$("body").on('click', '#close-menu', function(){
+$("body").on('click', '#close-menu', function() {
   $('.header-main').removeClass('open');
+});
+
+// modals
+
+$("#btn-modal-large").click(function() {
+  $("#modal-large").fadeIn(500);
+});
+$("#btn-modal-medium").click(function() {
+  $("#modal-medium").fadeIn(500);
+});
+$("#btn-modal-small").click(function() {
+  $("#modal-small").fadeIn(500);
+});
+$(".modal .close").click(function() {
+  $(".modal").fadeOut();
 });
