40feaf9b08b00f1837af4140425fece4dc88d404
Damián Muti <damianmuti@gmail.com>
2019-05-02 12:50:42 -0300

    Updated interaction

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,13 @@ $("body").on({
   mouseenter: function() {
     var submenu = $(this).find(".sub-menu");
 
-
-
     if (submenu && submenu[0].children.length) {
       $('.header-main').addClass('header-open');
     }
   },
   mouseleave: function() {
-    var submenu = $(this).find(".sub-menu");
+
     $('.header-main').removeClass('header-open');
-    if(submenu.parent('.submenu-item').find("a.active").length < 0)
-      $(this).find(".sub-menu").removeClass('open');
   }
 }, ".menu-main .submenu-item");
 
