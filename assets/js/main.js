695687b51c955f1bea6a17027a5020644af0c86c
Ignacio Rodrigues <ignaciorodriguesoficial@gmail.com>
2019-07-05 10:31:15 -0300

    Revert "fix(header): add classes instead of styles to hide header submenu"

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,13 +9,13 @@ $('body').on({
     if (submenu[0] && submenu[0].children.length) {
       $('.header-main').addClass('header-open');
     } else {
-      $('.sub-menu.open').removeClass('open');
+      $('.sub-menu.open').hide();
     }
   },
   mouseleave: function() {
     if ($('.header-main .submenu-item > a.active').length > 0){
       $('.header-main').addClass('header-open');
-      $('.header-main .submenu-item > a.active + .sub-menu').addClass('open');
+      $('.sub-menu.open').show(150);
     } else {
       $('.header-main').removeClass('header-open');
     }
