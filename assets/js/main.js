f55b03d57727a2e1d02c49964a14e6a45c21f7ca
gbaamonde <gusvodesign@gmail.com>
2020-01-21 10:33:55 -0300

    chore: add spaces

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,9 +84,9 @@ $(".show-hide").click(function() {
     $(this).prop('Counter',0).animate({
       Counter: $(this).text()
     },{
-      duration:3500,
-      easing:'swing',
-      step:function (now){
+      duration: 3500,
+      easing: 'swing',
+      step: function (now){
         $(this).text(Math.ceil(now).toFixed(1));
       }
     });
