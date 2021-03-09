5da70909ae886cf74b86f80e28014c754c875006
GusBaamonde <gusvodesign@gmail.com>
2021-03-08 23:48:32 -0300

    feat: add the switch component
    - add slide component
    - add the slide double component

--- assets/js/main.js
+++ assets/js/main.js
@@ -167,3 +167,28 @@ $('.progress-bar').each(function(index) {
     $(this).toggleClass('dp-open-results')
     $(this).closest('tr').next().toggleClass('show');
   });
+
+
+
+  /* Slide double TODO: Refactor this code */
+
+  document.getElementById("thumb-1").addEventListener ( "input", function(){
+    this.value=Math.min(this.value,this.parentNode.childNodes[5].value-1);
+    var value=(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.value)-(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.min);
+    var children = this.parentNode.childNodes[1].childNodes;
+    children[1].style.width=value+'%';
+    children[5].style.left=value+'%';
+    children[7].style.left=value+'%';children[11].style.left=value+'%';
+    children[11].childNodes[1].innerHTML=this.value;
+  });
+
+  document.getElementById("thumb-2").addEventListener ( "input", function(){
+    this.value=Math.max(this.value,this.parentNode.childNodes[3].value-(-1));
+    var value=(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.value)-(100/(parseInt(this.max)-parseInt(this.min)))*parseInt(this.min);
+    var children = this.parentNode.childNodes[1].childNodes;
+    children[3].style.width=(100-value)+'%';
+    children[5].style.right=(100-value)+'%';
+    children[9].style.left=value+'%';children[13].style.left=value+'%';
+    children[13].childNodes[1].innerHTML=this.value;
+  });
+
